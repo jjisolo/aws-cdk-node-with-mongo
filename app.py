77c05dd1c86e0712c5e54798f2433d66bbea79f0
jjisolo <jjisolo24@gmail.com>
2023-11-13 16:42:47 +0200

    f

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import aws_cdk as cdk
 from aws.ecs_stack import EcsStack
 
 app       = cdk.App()
-ecs_stack = EcsStack(app, "SirinNodeStack9")
+ecs_stack = EcsStack(app, "SirinNodeStack")
 app.synth()
